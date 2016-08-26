f5ea13beeb0bf8c15f607f10f0c4c26b14ba5ab8
Steven Clontz <steven.clontz@gmail.com>
2016-08-26 11:15:44 -0500

    playtesting revisions

--- shifting-message.rb
+++ shifting-message.rb
@@ -13,10 +13,10 @@ ciphertext = plaintext.split("").map do |char|
     index = index + 1
   end
   if index % 2 == 0 && ("A".."Z").include?(char)
-    new_char = (((new_char.ord - "A".ord - 11) % 26) + "A".ord).chr
+    new_char = (((new_char.ord - "A".ord - 4) % 26) + "A".ord).chr
   end
   if index % 3 == 0 && ("A".."Z").include?(char)
-    new_char = (((new_char.ord - "A".ord - 4) % 26) + "A".ord).chr
+    new_char = (((new_char.ord - "A".ord - 11) % 26) + "A".ord).chr
   end
   if index % 5 == 0 && ("A".."Z").include?(char)
     new_char = (((new_char.ord - "A".ord - 6) % 26) + "A".ord).chr
